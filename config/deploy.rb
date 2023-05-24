05b7cbfd4faaf61ac7158dcc5532703d901860e7
ale_ubuntu <alexisjebe@gmail.com>
2023-05-24 13:34:37 -0400

    arreglando el archivo deploy para subir

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,13 @@ lock "~> 3.17.3"
 set :application, "myapp"
 set :repo_url, "git@github.com:AlexisJeriaB/hogwarts.git"
 
+set :user, 'deploy'
+set :ssh_options, {
+    forward_agent: true,
+    auth_methods: ['publickey'],
+    keys: ['~/.ssh/aws_ruby_server.pem']
+}
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
